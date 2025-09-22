69c50c14aa2acde77bebc461617e95c39087276b
JoseRomualdo <jromualdo3@hotmail.com>
2025-09-22 09:33:03 -0300

    refactor(UserUpdateRequestDTO): remove Notnull and Notblank.

--- src/main/java/com/pomostudy/dto/user/UserUpdateRequestDTO.java
+++ src/main/java/com/pomostudy/dto/user/UserUpdateRequestDTO.java
@@ -4,11 +4,9 @@ import jakarta.validation.constraints.*;
 
 public record UserUpdateRequestDTO(
 
-        @NotBlank(message = "Name is required")
         @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
         String name,
 
-        @NotNull(message = "Password is required")
         @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$",
                 message = "The password should be contains at least one uppercase, one lowercase, one number and one special character")
         String password
