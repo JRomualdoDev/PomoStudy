b715f5b9386904dfb7b1a053f9f48ec7b278dec2
JoseRomualdo <jromualdo3@hotmail.com>
2025-09-24 12:18:30 -0300

    refactor(CategoryResponseDTO): add ID.

--- src/main/java/com/pomostudy/dto/category/CategoryResponseDTO.java
+++ src/main/java/com/pomostudy/dto/category/CategoryResponseDTO.java
@@ -2,9 +2,14 @@ package com.pomostudy.dto.category;
 
 import com.pomostudy.entity.Category;
 
-public record CategoryResponseDTO(String name, String color, String icon) {
+public record CategoryResponseDTO(Long id,String name, String color, String icon) {
 
     public CategoryResponseDTO(Category category) {
-        this(category.getName(), category.getColor(), category.getIcon());
+        this(
+                category.getId(),
+                category.getName(),
+                category.getColor(),
+                category.getIcon()
+        );
     }
 }
