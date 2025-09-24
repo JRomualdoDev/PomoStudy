f30bbbd7f96807440daa9126827a19e1e859f2f2
JoseRomualdo <jromualdo3@hotmail.com>
2025-09-24 12:21:22 -0300

    test(CategoryServiceTest): create a new test.

---
+++ src/test/java/com/pomostudy/service/CategoryServiceTest.java
@@ -0,0 +1,28 @@
+package com.pomostudy.service;
+
+import com.pomostudy.dto.category.CategoryMapper;
+import com.pomostudy.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private CategoryMapper categoryMapper;
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @BeforeEach
+    void setUp() {
+    }
+}
