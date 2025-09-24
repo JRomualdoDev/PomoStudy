f8a83152e1e411f96e026731ac68bdb446b3f1b4
JoseRomualdo <jromualdo3@hotmail.com>
2025-09-24 12:11:17 -0300

    refactor(MyApplication): createDate auto.

--- src/main/java/com/pomostudy/MyApplication.java
+++ src/main/java/com/pomostudy/MyApplication.java
@@ -2,9 +2,11 @@ package com.pomostudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class MyApplication {
 
     public static void main(String[] args) {
