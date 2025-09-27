a9d03b940a06c0426fcfd27d36c1bc3a0fbdaa4a
JoseRomualdo <jromualdo3@hotmail.com>
2025-09-26 21:58:34 -0300

    docs(GoalController): improve docs errors. refactor(GoalController): clean file extra code.

--- src/main/java/com/pomostudy/controller/GoalController.java
+++ src/main/java/com/pomostudy/controller/GoalController.java
@@ -1,24 +1,36 @@
 package com.pomostudy.controller;
 
 import com.pomostudy.config.security.SecurityConfigurations;
+import com.pomostudy.dto.ErrorResponseDTO;
 import com.pomostudy.dto.goal.GoalRequestDTO;
 import com.pomostudy.dto.goal.GoalResponseDTO;
 import com.pomostudy.entity.Goal;
+import com.pomostudy.exception.ResourceException;
 import com.pomostudy.exception.ResourceExceptionFactory;
 import com.pomostudy.service.GoalService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/goal")
-@Tag(name = "goal", description = "Controller for saving, edit, search and delete goal.")
+@Tag(name = "goal")
 @SecurityRequirement(name = SecurityConfigurations.SECURITY)
 public class GoalController {
 
@@ -31,7 +43,9 @@ public class GoalController {
     @PostMapping
     @Operation(summary = "Create data goal", description = "Method for create data goal")
     @ApiResponse(responseCode = "201", description = "Goal created with success")
-    @ApiResponse(responseCode = "400", description = "Invalid input data")
+    @ApiResponse(responseCode = "400", description = "Invalid input data",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<GoalResponseDTO> createGoal(@Valid @RequestBody GoalRequestDTO goalRequestDTO) {
 
         GoalResponseDTO goalResponseDTO = goalService.save(goalRequestDTO);
@@ -42,8 +56,12 @@ public class GoalController {
     @PutMapping("{id}")
     @Operation(summary = "Edit data goal", description = "Method for edit data goal")
     @ApiResponse(responseCode = "200", description = "Goal edited with success")
-    @ApiResponse(responseCode = "404", description = "Goal not found")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "404", description = "Goal not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<GoalResponseDTO> editGoal(@Valid @RequestBody GoalRequestDTO goalRequestDTO, @PathVariable("id") Long id) {
 
         GoalResponseDTO goalResponseDTO = goalService.edit(goalRequestDTO, id);
@@ -54,35 +72,61 @@ public class GoalController {
     @GetMapping
     @Operation(summary = "List all data goal", description = "Method for list data goal")
     @ApiResponse(responseCode = "200", description = "Goal listed successfully")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
-    public ResponseEntity<List<GoalResponseDTO>> findAllGoals() {
-        List<Goal> goals = goalService.findAll();
-        List<GoalResponseDTO> responseDTO = goals.stream()
-                .map(GoalResponseDTO::new)
-                .toList();
-        return ResponseEntity.ok(responseDTO);
+    @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
+    public ResponseEntity<Page<GoalResponseDTO>> findAllGoals(
+            @Parameter(
+                    name = "pageable",
+                    in = ParameterIn.QUERY,
+                    description = "Pagination and sorting object. **To use default values, send an empty object: `{}`**.",
+                    examples = {
+                            @ExampleObject(
+                                    name = "Default Pagination",
+                                    summary = "Fetch with default values",
+                                    value = "{}"
+                            ),
+                            @ExampleObject(
+                                    name = "Custom Pagination",
+                                    summary = "Fetching the first page with 10 items",
+                                    value = "{\"page\": 0, \"size\": 10, \"sort\": \"name,asc\"}"
+                            )
+                    },
+                    schema = @Schema(type = "object")
+            )
+            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)
+            Pageable pageable
+    ){
+        Page<GoalResponseDTO> listGoals = goalService.findAll(pageable);
+
+        return ResponseEntity.ok(listGoals);
     }
 
     @GetMapping("{id}")
     @Operation(summary = "Find data goal for id", description = "Method for search data goal for the id")
     @ApiResponse(responseCode = "200", description = "Goal listed successfully")
-    @ApiResponse(responseCode = "404", description = "Goal id not found")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "404", description = "Goal id not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<GoalResponseDTO> findGoalById(@PathVariable("id") Long id) {
-        GoalResponseDTO goalResponseDTO = goalService.findById(id)
-                .map(GoalResponseDTO::new)
-                .orElseThrow(() -> ResourceExceptionFactory.notFound("Goal", id));
-        return ResponseEntity.ok(goalResponseDTO);
+
+        return ResponseEntity.ok(goalService.findById(id));
     }
 
     @DeleteMapping("{id}")
     @Operation(summary = "Delete data goal for id", description = "Method for deleting data goal for the id")
     @ApiResponse(responseCode = "204", description = "No content")
-    @ApiResponse(responseCode = "404", description = "Goal not found")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "404", description = "Goal not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<String> deleteGoal(@PathVariable("id") Long id) {
-        goalService.findById(id)
-                .orElseThrow(() -> ResourceExceptionFactory.notFound("Goal", id));
+
         goalService.delete(id);
         return ResponseEntity.noContent().build();
     }
