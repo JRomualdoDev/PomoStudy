0e0f54a3bbd601e0473ecdc1fa5607c2269ade2c
JoseRomualdo <jromualdo3@hotmail.com>
2025-09-27 16:34:08 -0300

    refactor(GoalResponseDTO): ID add to constructor.

--- src/main/java/com/pomostudy/dto/goal/GoalResponseDTO.java
+++ src/main/java/com/pomostudy/dto/goal/GoalResponseDTO.java
@@ -5,12 +5,12 @@ import com.pomostudy.enums.GoalType;
 
 import java.time.OffsetDateTime;
 
-public record GoalResponseDTO(String title, String description, GoalType type,
+public record GoalResponseDTO(Long id, String title, String description, GoalType type,
                               Integer goalValue, Integer goalActual, OffsetDateTime endDate,
                               Boolean active) {
 
     public GoalResponseDTO(Goal goal) {
-        this(goal.getTitle(), goal.getDescription(), goal.getType(), goal.getGoalValue(),
+        this(goal.getId(), goal.getTitle(), goal.getDescription(), goal.getType(), goal.getGoalValue(),
                 goal.getGoalActual(), goal.getEndDate(), goal.getActive());
     }
 }
