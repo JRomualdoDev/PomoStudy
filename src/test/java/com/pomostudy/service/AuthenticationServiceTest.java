ab6ea091c2ac46907f418e583a12e9ed513e5822
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-09 09:38:37 -0300

    test(AuthenticationServiceTest): new file

---
+++ src/test/java/com/pomostudy/service/AuthenticationServiceTest.java
@@ -0,0 +1,71 @@
+package com.pomostudy.service;
+
+import com.pomostudy.entity.User;
+import com.pomostudy.enums.UserRole;
+import com.pomostudy.exception.ResourceException;
+import com.pomostudy.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthenticationServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    private User user;
+
+
+    @BeforeEach
+    void setUp() {
+       user = new User(
+                "Test",
+                "test@test.com",
+                "Abc@abc",
+                UserRole.USER
+        );
+
+    }
+
+    @Test
+    @DisplayName("Should return UserDetails when user is found for email")
+    void shouldReturnUserDetailsWhenUserIsFoundForEmail() {
+        String email = "test@test.com";
+
+        when(userRepository.findUserByEmail(email)).thenReturn(Optional.ofNullable(this.user));
+
+        UserDetails userDetails = authenticationService.loadUserByUsername(email);
+
+        assertNotNull(userDetails, "UserDetails should not be null");
+        assertEquals(email, userDetails.getUsername(), "The username should be the same as email");
+
+        verify(userRepository, times(1)).findUserByEmail(email);
+    }
+
+    @Test
+    @DisplayName("Should be throw ResourceException when user is not found")
+    void shouldThrowUsernameNotFoundExceptionWhenUserIsNotFound() {
+        String notExistingEmail = "abc@abc.com";
+
+        when(userRepository.findUserByEmail(notExistingEmail)).thenThrow(UsernameNotFoundException.class);
+
+        assertThrows(UsernameNotFoundException.class, () -> authenticationService.loadUserByUsername(notExistingEmail));
+
+        verify(userRepository, times(1)).findUserByEmail(notExistingEmail);
+    }
+}
