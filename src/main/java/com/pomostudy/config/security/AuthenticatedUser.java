9ae3d209bc514ec4f3fe8ddeeb4b5f9863396c04
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-09 09:03:37 -0300

    feat(AuthenticatedUser): new File

---
+++ src/main/java/com/pomostudy/config/security/AuthenticatedUser.java
@@ -0,0 +1,70 @@
+package com.pomostudy.config.security;
+
+import com.pomostudy.entity.User;
+import com.pomostudy.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AuthenticatedUser implements UserDetails {
+
+    private final User user;
+
+    public AuthenticatedUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    // Get Authority
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (user.getRole() == UserRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    // isAdmin ???
+    public boolean isAdmin() {
+        return getAuthorities()
+                .stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+    }
+
+}
