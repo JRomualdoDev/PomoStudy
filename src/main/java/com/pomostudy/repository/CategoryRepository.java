5b1f474ce6fcaf0d80f36e4a7575d94fa8a8636e
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-09 09:36:54 -0300

    refactor(CategoryRepository): findByUser

--- src/main/java/com/pomostudy/repository/CategoryRepository.java
+++ src/main/java/com/pomostudy/repository/CategoryRepository.java
@@ -1,9 +1,17 @@
 package com.pomostudy.repository;
 
+import com.pomostudy.config.security.AuthenticatedUser;
 import com.pomostudy.entity.Category;
+import com.pomostudy.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Page<Category> findByUser(User user, Pageable pageable);
 }
