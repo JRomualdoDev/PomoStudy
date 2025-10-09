a0c216aebcb9d4f8692753dd3310a92f93ad1dd7
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-09 09:27:44 -0300

    refactor(UserRepository): remove optional

--- src/main/java/com/pomostudy/repository/UserRepository.java
+++ src/main/java/com/pomostudy/repository/UserRepository.java
@@ -2,11 +2,16 @@ package com.pomostudy.repository;
 
 import com.pomostudy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    UserDetails findUserByEmail(String email);
+    Optional<User> findUserByEmail(String email);
+
 }
