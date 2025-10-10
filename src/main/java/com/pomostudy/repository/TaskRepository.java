36a8086af86e4b43699072364aace74ac7b06495
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-10 10:06:25 -0300

    refactor(TaskRepository): add findByUser.

--- src/main/java/com/pomostudy/repository/TaskRepository.java
+++ src/main/java/com/pomostudy/repository/TaskRepository.java
@@ -1,10 +1,15 @@
 package com.pomostudy.repository;
 
 import com.pomostudy.entity.Task;
+import com.pomostudy.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Page<Task> findByUser(User user, Pageable pageable);
 
 }
