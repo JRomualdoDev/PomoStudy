a290de9104fb3d781657c7fea4e46943cd75d35c
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-11 08:47:42 -0300

    refactor(GoalRepository): add findByUser

--- src/main/java/com/pomostudy/repository/GoalRepository.java
+++ src/main/java/com/pomostudy/repository/GoalRepository.java
@@ -1,10 +1,16 @@
 package com.pomostudy.repository;
 
 import com.pomostudy.entity.Goal;
+import com.pomostudy.entity.Task;
+import com.pomostudy.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
+
+    Page<Goal> findByUser(User user, Pageable pageable);
 
 }
