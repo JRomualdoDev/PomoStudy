9f141c45ecb82e9a1a55e3bbc23e03ea8d9635a2
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-11 08:50:55 -0300

    test(GoalServiceTest): add authenticated user

--- src/test/java/com/pomostudy/service/GoalServiceTest.java
+++ src/test/java/com/pomostudy/service/GoalServiceTest.java
@@ -1,6 +1,7 @@
 package com.pomostudy.service;
 
 
+import com.pomostudy.config.security.AuthenticatedUser;
 import com.pomostudy.dto.goal.GoalRequestDTO;
 import com.pomostudy.dto.goal.GoalResponseDTO;
 import com.pomostudy.entity.Goal;
@@ -40,8 +41,16 @@ class GoalServiceTest {
     @Mock
     private GoalMapper goalMapper;
 
+    @Mock
+    private AuthenticatedUser authenticatedUser;
+
+    @Mock
+    private AuthorizationService authorizationService;
+
     @InjectMocks
     private GoalService goalService;
+
+    private User user;
 
     private GoalRequestDTO goalRequestDTO;
     private GoalResponseDTO goalResponseDTO;
@@ -52,7 +61,7 @@ class GoalServiceTest {
     @BeforeEach
     void setUp() {
 
-        User user = new User();
+        user = new User();
         user.setId(1L);
         user.setName("test");
         user.setEmail("test@example.com");
@@ -75,8 +84,7 @@ class GoalServiceTest {
                 2,
                 1,
                 endDate,
-                true,
-                1L
+                true
         );
 
         goalResponseDTO = new GoalResponseDTO(
@@ -95,11 +103,11 @@ class GoalServiceTest {
     @DisplayName("Should save Goal with success in the db")
     void shouldSaveGoalSuccessfully() {
 
-        when(goalMapper.toCreateGoal(any(GoalRequestDTO.class))).thenReturn(goal);
+        when(goalMapper.toCreateGoal(any(GoalRequestDTO.class), any(AuthenticatedUser.class))).thenReturn(goal);
         when(goalRepository.save(any(Goal.class))).thenReturn(goal);
         when(goalMapper.toGoalResponseDTO(any(Goal.class))).thenReturn(goalResponseDTO);
 
-        GoalResponseDTO result = goalService.save(goalRequestDTO);
+        GoalResponseDTO result = goalService.save(goalRequestDTO, authenticatedUser);
 
         assertNotNull(result);
         assertEquals("Goal test", result.title());
@@ -111,7 +119,7 @@ class GoalServiceTest {
         assertEquals(true, result.active());
 
 
-        verify(goalMapper, times(1)).toCreateGoal(any(GoalRequestDTO.class));
+        verify(goalMapper, times(1)).toCreateGoal(any(GoalRequestDTO.class), any(AuthenticatedUser.class));
         verify(goalRepository, times(1)).save(any(Goal.class));
         verify(goalMapper, times(1)).toGoalResponseDTO(any(Goal.class));
     }
@@ -120,12 +128,12 @@ class GoalServiceTest {
     @DisplayName("Should throw exception when user is not found")
     void shouldThrowExceptionWhenUserNotFound() {
 
-        when(goalMapper.toCreateGoal(any(GoalRequestDTO.class))).thenThrow(ResourceExceptionFactory.notFound("User", goalRequestDTO.user_goal()));
+        when(goalMapper.toCreateGoal(any(GoalRequestDTO.class), any(AuthenticatedUser.class))).thenThrow(ResourceExceptionFactory.notFound("User", user.getId()));
 
-        ResourceException error = assertThrows(ResourceException.class, () -> goalService.save(goalRequestDTO));
+        ResourceException error = assertThrows(ResourceException.class, () -> goalService.save(goalRequestDTO, authenticatedUser));
         assertEquals("User with id 1 not found.", error.getMessage());
 
-        verify(goalMapper, times(1)).toCreateGoal(goalRequestDTO);
+        verify(goalMapper, times(1)).toCreateGoal(goalRequestDTO, authenticatedUser);
         verify(goalRepository, never()).save(any(Goal.class));
     }
 
@@ -133,11 +141,13 @@ class GoalServiceTest {
     @DisplayName("Should edit goal successfully in the db")
     void shouldEditGoalSuccessfully() {
         Long goalId = 1L;
-        when(goalMapper.toUpdateGoal(goalRequestDTO, goalId)).thenReturn(goal);
+        when(authenticatedUser.getUser()).thenReturn(user);
+        when(authorizationService.isOwner(Goal.class, goalId, user.getId())).thenReturn(true);
+        when(goalMapper.toUpdateGoal(goalRequestDTO, authenticatedUser, goalId)).thenReturn(goal);
         when(goalRepository.save(any(Goal.class))).thenReturn(goal);
         when(goalMapper.toGoalResponseDTO(any(Goal.class))).thenReturn(goalResponseDTO);
 
-        GoalResponseDTO result = goalService.edit(goalRequestDTO, goalId);
+        GoalResponseDTO result = goalService.edit(goalRequestDTO, authenticatedUser, goalId);
 
         assertNotNull(result);
         assertEquals("Goal test", result.title());
@@ -148,7 +158,7 @@ class GoalServiceTest {
         assertEquals(endDate, result.endDate());
         assertEquals(true, result.active());
 
-        verify(goalMapper, times(1)).toUpdateGoal(goalRequestDTO, goalId);
+        verify(goalMapper, times(1)).toUpdateGoal(goalRequestDTO, authenticatedUser,goalId);
         verify(goalRepository, times(1)).save(goal);
         verify(goalMapper, times(1)).toGoalResponseDTO(goal);
     }
@@ -158,13 +168,16 @@ class GoalServiceTest {
     void shouldThrowExceptionWhenUserNotBelongToGoal() {
 
         Long goalId = 1L;
-        when(goalMapper.toUpdateGoal(goalRequestDTO, goalId)).thenThrow(ResourceExceptionFactory.notFound("Goal", goalRequestDTO.user_goal()));
 
-        ResourceException error = assertThrows(ResourceException.class, () -> goalService.edit(goalRequestDTO, goalId));
+        when(authenticatedUser.getUser()).thenReturn(user);
+        when(authorizationService.isOwner(Goal.class, goalId, user.getId())).thenReturn(true);
+        when(goalMapper.toUpdateGoal(goalRequestDTO, authenticatedUser, goalId)).thenThrow(ResourceExceptionFactory.notFound("Goal", user.getId()));
+
+        ResourceException error = assertThrows(ResourceException.class, () -> goalService.edit(goalRequestDTO, authenticatedUser, goalId));
 
         assertEquals("Goal with id 1 not found.", error.getMessage());
 
-        verify(goalMapper, times(1)).toUpdateGoal(goalRequestDTO, goalId);
+        verify(goalMapper, times(1)).toUpdateGoal(goalRequestDTO, authenticatedUser, goalId);
         verify(goalRepository, never()).save(any(Goal.class));
     }
 
@@ -173,43 +186,75 @@ class GoalServiceTest {
     void shouldThrowExceptionWhenGoalNotFound() {
 
         Long goalId = 1L;
-        when(goalMapper.toUpdateGoal(goalRequestDTO, goalId)).thenThrow(ResourceExceptionFactory.notFound("Goal", goalId));
-
-        ResourceException error = assertThrows(ResourceException.class, () -> goalService.edit(goalRequestDTO, goalId));
+        when(authenticatedUser.getUser()).thenReturn(user);
+        when(authorizationService.isOwner(Goal.class, goalId, user.getId())).thenReturn(true);
+        when(goalMapper.toUpdateGoal(goalRequestDTO, authenticatedUser, goalId)).thenThrow(ResourceExceptionFactory.notFound("Goal", goalId));
+        ResourceException error = assertThrows(ResourceException.class, () -> goalService.edit(goalRequestDTO, authenticatedUser, goalId));
 
         assertEquals("Goal with id 1 not found.", error.getMessage());
 
-        verify(goalMapper, times(1)).toUpdateGoal(goalRequestDTO, goalId);
+        verify(goalMapper, times(1)).toUpdateGoal(goalRequestDTO, authenticatedUser, goalId);
         verify(goalRepository, never()).save(any(Goal.class));
     }
 
     @Test
-    @DisplayName("Should be pass correct pagination parameters to service and return 200 OK")
-    void shouldFindAllGoalDBWithStatus200() {
+    @DisplayName("Should find all goals for an ADMIN user and return 200 OK")
+    void shouldFindAllGoalsForAdmin() {
 
         List<Goal> goalResponse = Collections.singletonList(goal);
-        Pageable pageable = PageRequest.of(0,10);
+        Pageable pageable = PageRequest.of(0, 10);
+
+
+        when(authenticatedUser.isAdmin()).thenReturn(true);
 
         when(goalRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(goalResponse));
         when(goalMapper.toGoalResponseDTO(goal)).thenReturn(goalResponseDTO);
 
-        Page<GoalResponseDTO> result = goalService.findAll(pageable);
+
+        Page<GoalResponseDTO> result = goalService.findAll(pageable, authenticatedUser);
+
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.getContent().size());
+        verify(goalRepository, times(1)).findAll(any(Pageable.class));
+        verify(goalRepository, never()).findByUser(any(), any());
+    }
+
+    @Test
+    @DisplayName("Should find goals for a NON-ADMIN user and return 200 OK")
+    void shouldFindAllGoalsDBWithStatus200() {
+
+        List<Goal> goalResponse = Collections.singletonList(goal);
+        Pageable pageable = PageRequest.of(0,10);
+
+        when(authenticatedUser.isAdmin()).thenReturn(false);
+        when(authenticatedUser.getUser()).thenReturn(user);
+
+        when(goalRepository.findByUser(user, pageable)).thenReturn(new PageImpl<>(goalResponse));
+        when(goalMapper.toGoalResponseDTO(goal)).thenReturn(goalResponseDTO);
+
+        Page<GoalResponseDTO> result = goalService.findAll(pageable, authenticatedUser);
 
         assertFalse(result.isEmpty());
         assertEquals(1, result.getContent().size());
         assertEquals(goalResponseDTO,result.getContent().getFirst());
         assertEquals(goalRequestDTO.title(), result.getContent().getFirst().title());
+        assertEquals(goalRequestDTO.description(), result.getContent().getFirst().description());
+        assertEquals(goalRequestDTO.type(), result.getContent().getFirst().type());
 
-        verify(goalRepository, times(1)).findAll(any(Pageable.class));
+        verify(goalRepository, never()).findAll(pageable);
+        verify(goalRepository, times(1)).findByUser(user, pageable);
     }
 
     @Test
     @DisplayName("Should find the goal from id when the goal exist in the db")
     void shouldFindGoalFromIdWhenGoalExistDB() {
         Long goalId = 1L;
+        when(authenticatedUser.getUser()).thenReturn(user);
+        when(authorizationService.isOwner(Goal.class, goalId, user.getId())).thenReturn(true);
         when(goalRepository.findById(goalId)).thenReturn(Optional.of(goal));
 
-        Optional<GoalResponseDTO> result = Optional.ofNullable(goalService.findById(goalId));
+        Optional<GoalResponseDTO> result = Optional.ofNullable(goalService.findById(goalId,authenticatedUser));
 
         assertTrue(result.isPresent());
         assertEquals(goalResponseDTO, result.get());
@@ -221,9 +266,11 @@ class GoalServiceTest {
     @DisplayName("Should get Exception when do not find goal from id")
     void shouldGetExceptionWhenNotFindGoalFromId() {
         Long goalId = 99L;
+        when(authenticatedUser.getUser()).thenReturn(user);
+        when(authorizationService.isOwner(Goal.class, goalId, user.getId())).thenReturn(true);
         when(goalRepository.findById(goalId)).thenThrow(ResourceExceptionFactory.notFound("Goal", goalId));
 
-        ResourceException error = assertThrows(ResourceException.class, () -> goalService.findById(goalId));
+        ResourceException error = assertThrows(ResourceException.class, () -> goalService.findById(goalId, authenticatedUser));
 
         assertEquals("Goal with id 99 not found.", error.getMessage());
 
@@ -234,10 +281,12 @@ class GoalServiceTest {
     @DisplayName("Should delete goal succefully from db")
     void shouldDeleteGoalSuccessfullyDB() {
         Long goalId = 1L;
+        when(authenticatedUser.getUser()).thenReturn(user);
+        when(authorizationService.isOwner(Goal.class, goalId, user.getId())).thenReturn(true);
         when(goalRepository.findById(goalId)).thenReturn(Optional.ofNullable(goal));
         doNothing().when(goalRepository).deleteById(goalId);
 
-        goalService.delete(goalId);
+        goalService.delete(goalId,authenticatedUser);
 
         verify(goalRepository, times(1)).deleteById(goalId);
     }
