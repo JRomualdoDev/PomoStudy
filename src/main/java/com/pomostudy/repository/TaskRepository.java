41914c507487a869c8fe45936f0ccc1d53efc060
JoseRomualdo <jromualdo3@hotmail.com>
2025-10-19 21:22:11 -0300

    refactor(TaskRepository): new query findAllTaskByMonth.

--- src/main/java/com/pomostudy/repository/TaskRepository.java
+++ src/main/java/com/pomostudy/repository/TaskRepository.java
@@ -5,9 +5,11 @@ import com.pomostudy.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.OffsetDateTime;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -15,4 +17,7 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findByUser(User user, Pageable pageable);
 
     Page<Task> findByCategoryId(Long categoryId, Pageable pageable);
+
+    @Query("SELECT t FROM Task t WHERE t.user.id = :userId AND t.startDate >= :start AND t.startDate < :end")
+    Page<Task> findAllTaskByMonth(@Param("userId") Long userId, @Param("start") OffsetDateTime start, @Param("end") OffsetDateTime end, Pageable pageable);
 }
