7acc18b9c85e60ad972a206af91d80ba114439db
JoseRomualdo <jromualdo3@hotmail.com>
2026-06-23 15:24:56 -0300

    refactor(TokenService): Clean imports unsed and change the catch exception for verification JWT.

--- src/main/java/com/pomostudy/service/TokenService.java
+++ src/main/java/com/pomostudy/service/TokenService.java
@@ -3,8 +3,8 @@ package com.pomostudy.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.pomostudy.config.security.AuthenticatedUser;
-import com.pomostudy.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -39,7 +39,7 @@ public class TokenService {
                     .build()
                     .verify(token)
                     .getSubject();
-        } catch (JWTCreationException exception) {
+        } catch (JWTVerificationException exception) {
             return "";
         }
     }
